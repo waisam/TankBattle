ba1c151188dbc3be961d03798681397ad369cb32
醉饮洗閑愁 <waisam@live.com>
2023-05-02 20:08:40 +0800

    app启动设置移到模块文件中，在import时即可自动执行

---
+++ cn/tank/__init__.py
@@ -0,0 +1,11 @@
+import os
+if os.name == 'nt':
+    # Windows系统设置pillow为image provider，避免窗口启动后闪退。
+    os.environ['KIVY_IMAGE'] = 'pil'
+from kivy.config import Config
+Config.setall('kivy', {
+    'default_font': [
+        # 解决无法显示中文字体的问题
+        '仿宋_GB2312', os.path.join(os.getcwd(), 'data/fonts/仿宋_GB2312.ttf')],
+})
+Config.set('input', 'mouse', 'mouse,disable_multitouch')
